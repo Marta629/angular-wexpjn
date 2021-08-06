386b21c3baeba8fa0dc6845cd5fe8d11f7e37216
Marta629 <m.pelusi1@campus.unimib.it>
2021-08-06 17:46:18 +0200

    3 files modified

--- src/app/recap/recap.component.ts
+++ src/app/recap/recap.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-recap',
   templateUrl: './recap.component.html',
@@ -8,12 +10,21 @@ import { CartService } from '../cart.service';
 })
 export class RecapComponent implements OnInit {
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
   constructor(
     private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
   }
 
+  confirm() {
+    window.alert('Your order has been submitted');
+    this.items = this.cartService.clearCart();
+  }
 }
